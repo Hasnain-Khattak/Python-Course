894c61ceda3e4660f9995a00e54de8ce3ca73932
Hasnain-Khattak <msaqibkhan987987@gmail.com>
2024-09-17 21:37:23 +0500

    Day 2

--- Day-2/variables.py
+++ Day-2/variables.py
@@ -32,9 +32,66 @@
 #
 # print(float(y))
 
-
-
 # name = age = 25
 # print(age)
 # print(name)
+
+# --------------------------------------------------------------------------------------
+
+# pi = 3.14
+# name = "$"
+#
+# print(type(pi))
+# print(type(name))
+
+# para = '''
+# mnoiwnvwe
+# kimwefkwemnf
+#
+# 'kmewflk
+#
+# '''
+# print(type(para))
+# print(para)
+
+# number = input("Enter any number: ")
+
+# print(int(number))
+# print(number)
+# print(type(int(number)))
+# print(type(number))
+
+# pi = 3.14
+#
+# game_on = False
+#
+# print(game_on)
+#
+# game_on = True
+# print(game_on)
+
+# name, age = input('Enter Your name: '), int(input('Enter Your Age: '))
+# print('His name is in ', type(name), "His age is in ", type(age))
+
+# name = 'Hasnain'.capitalize()
+# print(name == 'hasnain'.capitalize())
+
+# name = 'Hasnain'
+# print(name.lower())
+
+# name = 'Hasnain'
+# print(name != 'Ahmad')
+
+# salary = 2500000
+# print(salary == 200000)
+
+# ----------------------------------------------------
+
+salary = 250000
+# print(not(salary < 200000 or salary < 500000))
+
+students = 'Hasnain'
+
+if 'Hasnain' in students:
+    print(True)
 
