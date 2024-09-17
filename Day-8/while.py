dbb1362fb88ee857cc4bd8802bdbc02f527eee74
Hasnain-Khattak <msaqibkhan987987@gmail.com>
2024-09-17 21:37:10 +0500

    Day 8

---
+++ Day-8/while.py
@@ -0,0 +1,22 @@
+# students = ['Hasnain', 'AHmad', 'Hamza']
+#
+# for student in students:
+#     len_of_name = len(student)
+#     if len_of_name == 7:
+#         print(student[0])
+#         print(student)
+
+
+# i = 1
+#
+# while i < 3:
+#     print('Hi')
+#     i += 1
+
+
+# while True:
+#     name = input('Enter Your name: ').capitalize()
+#     print('How are you ? ' + name)
+#     break
+
+
