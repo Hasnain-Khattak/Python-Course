82066e60ff90d6ac6b895bb7734a462cb316a7a0
Hasnain-Khattak <msaqibkhan987987@gmail.com>
2024-09-19 14:17:25 +0500

    Day 8

---
+++ Day-9/function.py
@@ -0,0 +1,13 @@
+# def my_function():
+#     print('Hasnain')
+#
+# if 10 % 2 ==0:
+#     print()
+
+# total = 0
+# num = int(input("Enter a number (0 to stop): "))
+# while num != 0:
+#     total += num
+#     num = int(input("Enter a number (0 to stop): "))
+#
+# print("Total sum:", total)
